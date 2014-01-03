2db013194eef36c5f7bd5f5cb58793f498b83b0d
Ben Angel <benjamin.b2@gmail.com>
2014-01-03 15:19:40 -0800

    balanced braces problem

---
+++ balanced_braces.rb
@@ -0,0 +1,50 @@
+# Balanced Braces: Given a string of opening and closing parentheses, check whether it’s balanced. We have 3 types of parentheses: round brackets: (), square brackets: [], and curly brackets: {}. Assume that the string doesn’t contain any other character than these, no spaces words or numbers. Just to remind, balanced parentheses require every opening parenthesis to be closed in the reverse order opened. For example ‘([])’ is balanced but ‘([)]‘ is not.
+
+class Brace
+  MISMATCHES = ["[)","[}","{]","{)","(}", "(]"]
+  TYPES = ["{}", "()", "[]"]
+  OPEN_SIDES = ["{", "(", "["]
+  CLOSED_SIDES = ["}", ")", "]"]
+
+  attr_reader :expression
+
+  def initialize(expression)
+    @expression = expression
+  end
+
+  def balanced?
+    equal_num? && mismatches.empty? && !open_end? ? 1 : 0
+  end
+
+  private
+  def equal_num?
+    expression.count("(") == expression.count(")") &&
+    expression.count("{") == expression.count("}") &&
+    expression.count("[") == expression.count("]")
+  end
+
+  def mismatches
+    MISMATCHES.select{|mismatch| expression.include?(mismatch)}
+  end
+
+  def open_end?
+    CLOSED_SIDES.include?(expression[0]) || OPEN_SIDES.include?(expression[-1])
+  end
+end
+
+class BraceCollection
+  def initialize(braces)
+    @braces = braces.map{|brace_parts| Brace.new(brace_parts)}
+  end
+
+  def check_balance
+    @braces.map{|brace| brace.balanced? }
+  end
+end
+
+def check_braces(expressions)
+  braces = BraceCollection.new(expressions)
+  braces.check_balance
+end
+
+check_braces([")(){}","[]({})","([])","{()[]}","([)]"])
