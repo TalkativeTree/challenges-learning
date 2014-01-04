b99da4aed7079c666bc13d3b136dcde5adfced5e
Ben Angel <benjamin.b2@gmail.com>
2014-01-03 19:30:30 -0800

    added minitests

--- balanced_braces.rb
+++ balanced_braces.rb
@@ -1,8 +1,9 @@
 # Balanced Braces: Given a string of opening and closing parentheses, check whether it’s balanced. We have 3 types of parentheses: round brackets: (), square brackets: [], and curly brackets: {}. Assume that the string doesn’t contain any other character than these, no spaces words or numbers. Just to remind, balanced parentheses require every opening parenthesis to be closed in the reverse order opened. For example ‘([])’ is balanced but ‘([)]‘ is not.
 
+require "minitest/autorun"
 class Brace
-  MISMATCHES = ["[)","[}","{]","{)","(}", "(]"]
   TYPES = ["{}", "()", "[]"]
+  MISMATCHES = ["[)","[}","{]","{)","(}", "(]"]
   OPEN_SIDES = ["{", "(", "["]
   CLOSED_SIDES = ["}", ")", "]"]
 
@@ -47,4 +48,13 @@ def check_braces(expressions)
   braces.check_balance
 end
 
-check_braces([")(){}","[]({})","([])","{()[]}","([)]"])
+class TestBraces < Minitest::Test
+  def setup
+    @braces = [")(){}","[]({})","([])","{()[]}","([)]", "{(}{)({{(}{}{)}{}{)({})})}})})}", "{((()))[]}", "{", ")", "{sdasdafa[]}", "{sad)}"]
+    @results = [0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0]
+  end
+
+  def test_check_braces
+    assert_equal @results, check_braces(@braces)
+  end
+end
