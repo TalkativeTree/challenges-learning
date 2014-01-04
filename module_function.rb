feb40453118ea39ef48bc446993f690f64061ccd
Ben Angel <benjamin.b2@gmail.com>
2014-01-03 19:26:11 -0800

    added test to module_function

--- module_function.rb
+++ module_function.rb
@@ -1,24 +1,7 @@
-module Functions
-  module_function
+require "minitest/autorun"
 
-  def puts_string(string)
-    puts string
-  end
-end
-
-class Printer
-  include Functions
-
-  def print(string = '')
-    printer.invoke(self, string)
-  end
-
-  def printer
-    Function.new(:puts_string)
-  end
-
-end
-
+# Written to learn how the Chronic gem uses handlers and module_function
+# to check strings against handler definitions of dates/time and return correct times.
 class Function
   def initialize(method, options = {})
     @handler_method = method
@@ -27,5 +10,54 @@ class Function
 
   def invoke(putter, string)
     putter.send(@handler_method, string)
+  end
+end
+
+module Functions
+  module_function
+
+  def puts_string(string)
+    p string
+  end
+
+  def puts_ten(string)
+    Array.new(10){string}
+  end
+
+  def capitalize(string)
+    p string.upcase
+  end
+end
+
+class Printer
+  include Functions
+
+  def print(string = '')
+    printer_jobs.map{ |job| job.invoke(self, string) }
+  end
+
+  def printer_jobs
+    [
+      Function.new(:puts_string),
+      Function.new(:capitalize),
+      Function.new(:puts_ten)
+    ]
+  end
+
+end
+
+class PrinterTest < Minitest::Test
+  def setup
+    @printer = Printer.new
+    @print_results = ["test this",
+                      "TEST THIS",
+                      ["test this", "test this", "test this", "test this",
+                       "test this", "test this", "test this", "test this",
+                       "test this", "test this"]
+                      ]
+  end
+
+  def test_print
+    assert_equal @print_results, Printer.new.print("test this")
   end
 end
