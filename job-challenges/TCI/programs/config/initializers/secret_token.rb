a9984b5677903f0b551752733ac70a075d4d1f38
Ben Angel <benjamin.b2@gmail.com>
2014-02-14 20:06:30 -0800

    initial commit

---
+++ job-challenges/TCI/programs/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Programs::Application.config.secret_key_base = 'e4e66b92efa153544555eb7d5d806ce5c4bc2cc12eacbb9f5374d012dc6303e40fc24fb008f52a2bc4169198cb750108019e73f3e10e98641c9100f8d1d8a6df'
