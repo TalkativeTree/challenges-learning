516f1e30c3bd9f28130e3af93f574c8a1f8b49ee
Ben Angel <benjamin.b2@gmail.com>
2014-02-18 23:43:03 -0800

    add raising errors to Stack#push and Stack#pop.

--- job-challenges/TCI/stack/stack.rb
+++ job-challenges/TCI/stack/stack.rb
@@ -11,9 +11,16 @@ class Stack
   end
 
   def push(item)
-    items.push(item) if level != items.count
+    raise "Error: stack overflow." if size == level
+
+    #alternative is items << item if level !=items
+    items.push(item)
   end
 
   def pop
+    raise "Error: stack underflow." if size == 0
+
+    #alternative is items.delete_at(-1)
+    items.pop
   end
 end
