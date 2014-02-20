3c03ac42f4da46d33c0bd7bfc03d6ba2c825e13a
Ben Angel <benjamin.b2@gmail.com>
2014-02-19 17:19:34 -0800

    add route resources for Programs and root too Programs#index

--- job-challenges/TCI/challenge/config/routes.rb
+++ job-challenges/TCI/challenge/config/routes.rb
@@ -3,7 +3,8 @@ Challenge::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'programs#index'
+  resources :programs
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
