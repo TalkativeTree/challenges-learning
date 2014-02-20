f5ced9fc3c5e4fecc5b722e83cc2edd6009dce96
Ben Angel <benjamin.b2@gmail.com>
2014-02-19 17:17:47 -0800

    add validations for Programs#title, #subtitle, and #code for presence, format, and #code length

--- job-challenges/TCI/challenge/app/models/programs.rb
+++ job-challenges/TCI/challenge/app/models/programs.rb
@@ -1,2 +1,9 @@
 class Programs < ActiveRecord::Base
+  validates :code, length: { is: 6 }
+
+  [:title, :subtitle, :code].each do |column|
+
+    validates_presence_of column
+    validates_format_of column, with: /\w/, message: "Only letters or numbers allowed."
+  end
 end
