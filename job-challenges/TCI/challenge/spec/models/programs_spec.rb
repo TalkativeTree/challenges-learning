efba2ed0615bddd8df36a12576f9216bdd8e6b47
Ben Angel <benjamin.b2@gmail.com>
2014-02-19 17:18:27 -0800

    add spec and validations for Programs model

--- job-challenges/TCI/challenge/spec/models/programs_spec.rb
+++ job-challenges/TCI/challenge/spec/models/programs_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Programs do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should ensure_length_of(:code).is_equal_to(6)}
+
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:subtitle) }
+  it { should validate_presence_of(:code) }
+
+  it { should allow_value("ABsIES").for(:title) }
+  it { should allow_value("ABsIES").for(:subtitle) }
+  it { should allow_value("ABsIES").for(:code) }
+
+  it { should_not allow_value("&(*@").for(:title) }
+  it { should_not allow_value(")@(#{}").for(:subtitle) }
+  it { should_not allow_value("*&#}{").for(:code) }
+  it { should_not allow_value("12345").for(:code)}
 end
